62a3da5b23f2582980ca52582d061ddc097d95f6
Misraim <faustomisrainpoot@gmail.com>
2022-03-07 05:24:18 -0500

    Adaptador personalizado

--- app/src/main/java/com/example/gridview/MainActivity.kt
+++ app/src/main/java/com/example/gridview/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.gridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var frutas = ArrayList<Fruta>()
+        frutas.add(Fruta("Manzana", R.drawable.manzana))
+        frutas.add(Fruta("Plátano", R.drawable.platano))
+        frutas.add(Fruta("Sandía", R.drawable.sandia))
+        frutas.add(Fruta("Durasno", R.drawable.durazno))
+
+        var grid:GridView = findViewById(R.id.grid)
+
+        val adaptador = AdaptadorCustom(this, frutas)
+        grid.adapter =adaptador
+
+        grid.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, frutas.get(position).nombre, Toast.LENGTH_SHORT).show()
+        }
     }
 }
